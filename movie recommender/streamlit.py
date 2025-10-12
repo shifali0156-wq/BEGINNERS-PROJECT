139947a8f058fb56bf2cfe451aff2952e4c80a9b
shifali0156-wq <shifali.0156@gmail.com>
2025-10-12 23:17:28 +0530

    first streamlit file

---
+++ movie recommender/streamlit.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+# --- Transform dataset ---
+data = pd.read_csv("movie.csv")
+counter=CountVectorizer(max_features=4000, stop_words='english')
+vectors=counter.fit_transform(data.tags).toarray()
+similarity=cosine_similarity(vectors)
+
+indexed_list=[]
+for i in similarity:
+  movie=[]
+  for index,j in enumerate(i):
+    movie.append((index,float(j)))
+  indexed_list.append(movie)
+
+# --- Recommendation logic ---
+def recommend(movie):
+    movie_index=data[data.title_x==movie].index[0]
+    distances=indexed_list[movie_index]
+    movies_list=sorted(distances,reverse=True,key=lambda x:x[1])[1:11]
+    movies_names=[]
+    for i in movies_list:
+        movies_names.append(data.iloc[i[0]].title_x)
+    return movies_names
+
+
+# --- Streamlit UI ---
+st.title("🎬 Movie Recommendation System")
+selected_movie = st.selectbox("Select a movie to get recommendations:", data.title_x)
+
+if st.button("Show Recommendations"):
+    results = recommend(selected_movie)
+    for row in results:
+        st.subheader(row)
+
+ 
+
